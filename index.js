b5da590a31f37b7dee5a5b4cc0b7ec85a8590866
iMatheusPaula <imatheuspaula@gmail.com>
2023-11-21 01:30:19 -0300

    CSS Produtos OK

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const num = 1;
+let buttonBuy = document.querySelectorAll('.buy');
+
+function btnFunc(event){
+    var item = document.getElementsByClassName('box').value;
+    console.log(item);
+}
+
+for(var i=0; i<buttonBuy.length; i++){
+    var button = buttonBuy[i];
+    button.addEventListener('click', btnFunc);
+}
